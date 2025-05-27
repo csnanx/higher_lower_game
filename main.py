8a2ad29225adb7d9eb5cbec39e79ad3032fdd9af
csnanx <210231094+csnanx@users.noreply.github.com>
2025-05-27 20:51:05 +0530

    Add logic to get user choice

--- main.py
+++ main.py
@@ -11,3 +11,12 @@ person_2 = get_random_person()
 
 print(f"Compare A: {person_1['name']}, a {person_1['description']}, from {person_1['country']}")
 print(f"Compare B: {person_2['name']}, a {person_2['description']}, from {person_2['country']}")
+
+user_choice = input("Who has more followers? Type 'A' or 'B': ").upper()
+
+if user_choice == "A":
+    user_choice = person_1
+elif user_choice == "B":
+    user_choice = person_2
+
+print(user_choice)
