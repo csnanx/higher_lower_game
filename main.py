d32c7a147d6ae5af495d59d00e42f0fe5b1fee02
csnanx <210231094+csnanx@users.noreply.github.com>
2025-05-27 21:00:19 +0530

    Add code to check if user choice is right

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ def get_random_person():
 print(logo)
 person_1 = get_random_person()
 person_2 = get_random_person()
+score = 0
+
 
 print(f"Compare A: {person_1['name']}, a {person_1['description']}, from {person_1['country']}")
 print(f"Compare B: {person_2['name']}, a {person_2['description']}, from {person_2['country']}")
@@ -24,3 +26,10 @@ if person_1["follower_count"] > person_2["follower_count"]:
 else:
     more_followers = person_2
 
+if user_choice == more_followers:
+    score += 1
+    print("Yes. You are absolutely RIGHT!")
+else:
+    print(f"Oh no! That's wrong.")
+
+print(f"Final Score: {score}")
