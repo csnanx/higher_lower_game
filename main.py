2b01fda154674f7a69496519792f4040811f863f
csnanx <210231094+csnanx@users.noreply.github.com>
2025-05-27 20:44:46 +0530

    Print logo ASCII art to the screen

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from random import choice
 from game_data import data
+from art import logo, vs
 
 def get_random_person():
     return choice(data)
 
+print(logo)
 person_1 = get_random_person()
 person_2 = get_random_person()
+print(person_1)
+print(person_2)
